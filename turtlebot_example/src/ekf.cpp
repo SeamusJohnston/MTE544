ef97525d3c5a4c5a9d5ee5ff4b3319a0e5e3e893
Seamus Johnston <seamusbjohnston@gmail.com>
2020-03-13 12:09:10 -0400

    Updated EKF and rviz scripts

--- turtlebot_example/src/ekf.cpp
+++ turtlebot_example/src/ekf.cpp
@@ -4,7 +4,7 @@ EKF::EKF(float freq)
 : frequency(freq),
 dt_(1.f/freq),
 ips_sub_(
-        nh_.subscribe("ips",
+        nh_.subscribe("/indoor_pos",
                       1,
                       &EKF::IpsCallback,
                       this)),
@@ -14,7 +14,7 @@ odom_sub_(
                       &EKF::OdomCallback,
                       this)),
 control_sub_(
-        nh_.subscribe("/cmd_vel",
+        nh_.subscribe("/cmd_vel_mux/input/teleop",
                       10,
                       &EKF::ControlCallback,
                       this)),
@@ -26,25 +26,25 @@ est_pub_(
 gen_(rd_()),
 nd_(0, 0.1)    
 {
-  state_ << 0, 0, 0;
+  state_ << 3.07, -0.0138, 1.226;
   state_prev_ = state_;
-  P_ << 0, 0, 0, 
-        0, 0, 0, 
-        0, 0, 0;
-  R_ << 0.1, 0, 0,
-        0, 0.1, 0,
+  P_ << 0.0001, 0, 0, 
+        0, 0.0001, 0, 
+        0, 0, 0.0001;
+  R_ << 0.01, 0, 0,
+        0, 0.01, 0,
         0, 0, 0.01;
-  Q_ << 0.2, 0, 0, 0, 0, 
-        0, 0.2, 0, 0, 0,
+  Q_ << 0.1, 0, 0, 0, 0, 
+        0, 0.1, 0, 0, 0,
         0, 0, 0.1, 0, 0,
-        0, 0, 0, 0.2, 0, 
-        0, 0, 0, 0, 0.1;
+        0, 0, 0, 0.01, 0, 
+        0, 0, 0, 0, 0.25;
   // Initialize P, Q, R with config?
 }
 
 void EKF::IpsCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg) {
-  measurement_(int(MEASURE_IDX::x)) = msg->pose.pose.position.x; //+ nd_(gen_);
-  measurement_(int(MEASURE_IDX::y)) = msg->pose.pose.position.y; //+ nd_(gen_);
+  measurement_(int(MEASURE_IDX::x)) = msg->pose.pose.position.x + nd_(gen_);
+  measurement_(int(MEASURE_IDX::y)) = msg->pose.pose.position.y + nd_(gen_);
   tf::Quaternion q(
       msg->pose.pose.orientation.x,
       msg->pose.pose.orientation.y,
@@ -59,11 +59,6 @@ void EKF::IpsCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& 
 void EKF::OdomCallback(const nav_msgs::Odometry::ConstPtr& msg) {
   measurement_(int(MEASURE_IDX::v)) = msg->twist.twist.linear.x;
   measurement_(int(MEASURE_IDX::w)) = msg->twist.twist.angular.z;
-  // Use covariances?
-
-  // TODO: Remove this!
-  control_(int(CONTROL_IDX::v)) = msg->twist.twist.linear.x;
-  control_(int(CONTROL_IDX::w)) = msg->twist.twist.angular.z;
 }
 
 void EKF::ControlCallback(const geometry_msgs::Twist::ConstPtr& msg) {
@@ -79,15 +74,14 @@ void EKF::UpdateState() {
 }
 
 void EKF::UpdateMatrices() {
-  // TODO: Implement this
-  F_ << 1, 0, -control_(int(CONTROL_IDX::v))*sin(state_(int(STATE_IDX::theta))),
-        0, 1, control_(int(CONTROL_IDX::v))*cos(state_(int(STATE_IDX::theta))),
+  F_ << 1, 0, -dt_*control_(int(CONTROL_IDX::v))*sin(state_(int(STATE_IDX::theta))),
+        0, 1, dt_*control_(int(CONTROL_IDX::v))*cos(state_(int(STATE_IDX::theta))),
         0, 0, 1;
 
   float d = sqrt(pow(state_(int(STATE_IDX::x)),2) + pow(state_(int(STATE_IDX::y)),2));
   H_ << 1, 0, 0, 
         0, 1, 0,
-        state_(int(STATE_IDX::x))/(dt_*d), state_(int(STATE_IDX::y))/(dt_*d), 0, // May want to add back the offsets here
+        state_(int(STATE_IDX::x))/(dt_*d + pow(10,-9)), state_(int(STATE_IDX::y))/(dt_*d + pow(10,-9)), 0, // May want to add back the offsets here
         0, 0, 1,
         0, 0, 1/dt_; // May want to add back the offsets here too
 }
@@ -110,7 +104,7 @@ void EKF::Update() {
 
   P_ = (Eigen::Matrix3f::Identity() - K*H_)*P_;
 
-  ROS_INFO("State Est: x: %f, y: %f, theta: %f", state_(int(STATE_IDX::x)), state_(int(STATE_IDX::y)), state_(int(STATE_IDX::theta)));
+  ROS_INFO("State Est: x: %f, y: %f, theta: %f", state_(int(STATE_IDX::x)), state_(int(STATE_IDX::y)), state_(int(STATE_IDX::theta))*180/M_PI);
 
   Publish();
 }
@@ -127,6 +121,12 @@ void EKF::Publish() {
   msg.pose.orientation.y = q.y();
   msg.pose.orientation.z = q.z();
   msg.pose.orientation.w = q.w();
+  msg.covariance = {P_(0,0), P_(0,1), 0, 0, 0, P_(0,2),
+                    P_(1,0), P_(1,1), 0, 0, 0, P_(1,2),
+                    0, 0, pow(10,5), 0, 0, 0, 
+                    0, 0, 0, pow(10,5), 0, 0,
+                    0, 0, 0, 0, pow(10,5), 0,
+                    P_(2,0), P_(2,1), 0, 0, 0, P_(2,2)};
   est_pub_.publish(msg);
 }
 
---
+++ turtlebot_example/src/rviz_publisher.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+import rospy
+import math
+from geometry_msgs.msg import PoseStamped, PoseWithCovariance, PoseWithCovarianceStamped, Pose
+from nav_msgs.msg import Path
+from visualization_msgs.msg import Marker
+
+path_ips_pub = rospy.Publisher('ips_path', Path, queue_size=10)
+path_ekf_pub = rospy.Publisher('path_ekf', Path, queue_size=10)
+viz_ekf_pub = rospy.Publisher('ekf_viz', Marker, queue_size=10)
+
+path_ips = Path()
+path_ekf = Path()
+
+# TODO: Not sure if the paths/markers need frame info in order for rviz to work correctly
+
+def EKFCallback(msg):
+  global path_ekf
+  # Publish path (add most recent pose onto it)
+  pose_new = PoseStamped()
+  pose_new.pose = msg.pose
+  path_ekf.poses.append(pose_new)
+  path_ekf_pub.publish(path_ekf)
+
+  # Publish visualization ellipses
+  marker = Marker()
+  marker.type = Marker.CYLINDER
+  marker.action = Marker.ADD
+  marker.pose.position = msg.pose.position
+  marker.scale.x = msg.covariance[0]
+  marker.scale.y = msg.covariance[7]
+  marker.scale.z = 0
+  viz_ekf_pub.publish(marker)
+
+def IPSCallback(msg):
+  global path_ips
+  # Publish path (add most recent pose onto it)
+  pose_new = PoseStamped()
+  pose_new.pose = msg.pose.pose
+  path_ips.poses.append(pose_new)
+  path_ips_pub.publish(path_ips)
+
+if __name__ == '__main__':
+    rospy.init_node('rviz_publisher')
+    rospy.Subscriber('/pose_est',
+                     PoseWithCovariance,
+                     EKFCallback)
+    rospy.Subscriber('/indoor_pos',
+                     PoseWithCovarianceStamped,
+                     IPSCallback)          
+    rospy.spin()
