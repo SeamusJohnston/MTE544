2dbf5e105eb22fb51eedb55becb9c64c7cc8fbd4
Seamus Johnston <seamusbjohnston@gmail.com>
2020-03-13 14:50:09 -0400

    Updated publishing scripts, in process of tuning filter

--- turtlebot_example/src/rviz_publisher.py
+++ turtlebot_example/src/rviz_publisher.py
@@ -17,26 +17,36 @@ path_ekf = Path()
 def EKFCallback(msg):
   global path_ekf
   # Publish path (add most recent pose onto it)
+  path_ekf.header.frame_id = "map"
   pose_new = PoseStamped()
+  pose_new.header.frame_id = "map"
   pose_new.pose = msg.pose
   path_ekf.poses.append(pose_new)
   path_ekf_pub.publish(path_ekf)
 
   # Publish visualization ellipses
   marker = Marker()
+  marker.header.stamp = rospy.Time.now()
+  marker.header.frame_id = "map"
   marker.type = Marker.CYLINDER
   marker.action = Marker.ADD
   marker.pose.position = msg.pose.position
+  marker.pose.orientation.w = 1
   marker.scale.x = msg.covariance[0]
   marker.scale.y = msg.covariance[7]
-  marker.scale.z = 0
+  marker.scale.z = 0.001
+  marker.color.a = 1
+  marker
   viz_ekf_pub.publish(marker)
 
 def IPSCallback(msg):
   global path_ips
   # Publish path (add most recent pose onto it)
+  path_ips.header.frame_id = "map"
   pose_new = PoseStamped()
+  pose_new.header.frame_id = "map"
   pose_new.pose = msg.pose.pose
+  pose_new.pose.position.z = 0
   path_ips.poses.append(pose_new)
   path_ips_pub.publish(path_ips)
 
